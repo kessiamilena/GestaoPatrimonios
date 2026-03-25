9d249823add77a749b19b283d192a0630fb58dd9
kessiamilena <kessia.milena124@yahoo.com>
2026-03-25 11:44:47 -0300

    :tada: Commit inicial

---
+++ GestaoPatrimonios/Program.cs
@@ -0,0 +1,45 @@
+using DotNetEnv;
+using GestaoPatrimonios.Applications.Services;
+using GestaoPatrimonios.Contexts;
+using GestaoPatrimonios.Interfaces;
+using GestaoPatrimonios.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// carregando o .env
+Env.Load();
+
+// pegando a connection string
+string connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+
+// Conexão com banco
+builder.Services.AddDbContext<GestaoPatrimoniosContext>(options => options.UseSqlServer(connectionString));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Áreas
+builder.Services.AddScoped<IAreaRepository, AreaRepository>();
+builder.Services.AddScoped<AreaService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
