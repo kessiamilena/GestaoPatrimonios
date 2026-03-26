bab1308f17ecdca944bd05e7150d02689816bdcd
kessiamilena <kessia.milena124@yahoo.com>
2026-03-26 11:17:32 -0300

    :sparkles: feat: Adição das camadas de localização

---
+++ GestaoPatrimonios/DTOs/LocalizacaoDto/CriarLocalizacaoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace GestaoPatrimonios.DTOs.LocalizacaoDto
+{
+    public class CriarLocalizacaoDto
+    {
+        public string NomeLocal { get; set; } = string.Empty;
+        public int LocalSAP { get; set; }
+        public string DescricaoSAP { get; set; }
+        public Guid AreaID { get; set; }
+    }
+}
